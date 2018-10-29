98d712b7d431f84a555d1b307fc6c808d0529854
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-10-29 18:34:42 +0300

    Use Hunter to download OpenBLAS

--- cmake/ChooseBlas.cmake
+++ cmake/ChooseBlas.cmake
@@ -38,9 +38,18 @@ if(BLAS STREQUAL "Atlas" OR BLAS STREQUAL "atlas")
   add_definitions(-DMSHADOW_USE_CBLAS=1)
   add_definitions(-DMSHADOW_USE_MKL=0)
 elseif(BLAS STREQUAL "Open" OR BLAS STREQUAL "open")
+  if(HUNTER_ENABLED)
+    hunter_add_package(OpenBLAS)
+    find_package(OpenBLAS CONFIG REQUIRED)
+    list(APPEND mshadow_LINKER_LIBS OpenBLAS::OpenBLAS)
+  else()
+
   find_package(OpenBLAS REQUIRED)
   include_directories(SYSTEM ${OpenBLAS_INCLUDE_DIR})
   list(APPEND mshadow_LINKER_LIBS ${OpenBLAS_LIB})
+
+  endif()
+
   add_definitions(-DMSHADOW_USE_CBLAS=1)
   add_definitions(-DMSHADOW_USE_MKL=0)
 elseif(BLAS STREQUAL "MKL" OR BLAS STREQUAL "mkl")
